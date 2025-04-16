ffbd5ba50114ea2ee9568dab0847aecb6a4cfb4e
janki2425 <152633106+janki2425@users.noreply.github.com>
2025-04-16 18:24:15 +0530

    integrated events with the  backend api

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: '342b-110-226-17-132.ngrok-free.app',
-//         port: '',
-//         pathname: '/uploads/**', // Allows any path under /uploads
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const ngrokHost = process.env.NGROK_URL || '342b-110-226-17-132.ngrok-free.app';
@@ -23,7 +5,7 @@ const ngrokHost = process.env.NGROK_URL || '342b-110-226-17-132.ngrok-free.app';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Move unoptimized here inside the 'images' config
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
