3bc84f4582f60f81939bcf3c402695f4c5925f86
janki2425 <152633106+janki2425@users.noreply.github.com>
2025-04-29 15:58:12 +0530

    changed url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const ngrokHost = '4262-2401-4900-5047-50b9-1500-9254-60f2-170a.ngrok-free.app';
+const ngrokHost = 'e413-2401-4900-5047-50b9-1500-9254-60f2-170a.ngrok-free.app';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
